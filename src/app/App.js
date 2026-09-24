ea45691955b4d6b9a6900f0e549e1043bfce59cd
Rodrigo <eusourodrigo895@gmail.com>
2026-09-24 16:29:41 -0300

    fix/feat: swagger docs and fixes in swagger identation

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import routes from "../routes/index.js";
 
 const app = express();
+const swaggerOptions = { swaggerOptions: { url: "/swagger.yaml" } };
 
 app.use(express.json());
 app.use("/public", express.static("public"));
+app.get("/swagger.yaml", (req, res) => res.sendFile("/swagger.yaml", { root: "." }));
+app.use("/docs", swaggerUi.serveFiles(null, swaggerOptions), swaggerUi.setup(null, swaggerOptions));
 app.use(routes);
 app.use((erro, req, res, next) => {
   const status = erro.status || 500;
